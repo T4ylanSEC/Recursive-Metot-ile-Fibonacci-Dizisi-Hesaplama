1734651b4d9c8f74a9c57e092b19985554c74dc3
yaren <yaren@DESKTOP-9U2BG79>
2024-12-17 03:04:10 +0300

    Add project files.

---
+++ Recursive Metot ile Fibonacci Dizisi Hesaplama/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_Metot_ile_Fibonacci_Dizisi_Hesaplama
+{
+    internal class Program
+    {
+        static int Fibonacci(int n)
+        {
+            if (n <= 1) // Temel durum: n = 0 veya n = 1
+            {
+                return n;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Fibonacci Dizisindeki Eleman Sırasını Giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+
+            int sonuc = Fibonacci(sayi);
+            Console.WriteLine($"{sayi}. Fibonacci Sayısı: {sonuc}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
